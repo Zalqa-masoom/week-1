4dd7e7e2bbe171dac3237db36c51fd3e9316b1df
Zalqa <zalqamasoom02@gmail.com>
2026-07-19 18:57:19 +0500

    Enhance log counter script with parameters

--- my_log_counter.sh
+++ my_log_counter.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-count=$(grep -c "ERROR" my_log.txt)
-echo"We found $count errors in the file!"
+set -euo pipefail
+if [ "$#" -ne 2 ]; then
+echo "Usage: $0 [log_file] [pattern]"
+exit 1
+fi
+LOG_FILE=$1
+PATTERN=$2
+COUNT=$(grep -c "$PATTERN" "$LOG_FILE")
+echo "THE PATTERN '$PATTERN' appered $COUNT time in $LOG_FILE."
